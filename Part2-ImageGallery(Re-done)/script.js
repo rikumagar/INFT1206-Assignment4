3258bd221df6892a9adf6dbd4fb2f20ea15be020
rikumagar <84245137+rikumagar@users.noreply.github.com>
2025-11-28 00:00:33 -0500

    Second commit - THis is the second commit with HTML header and comments

--- Part2-ImageGallery(Re-done)/script.js
+++ Part2-ImageGallery(Re-done)/script.js
@@ -1,5 +1,12 @@
+/*
+  Name: Hrigdev Thapa
+  Date:2025/11/27
+  Filename: main.js
+  Description: This JavaScript file builds a simple image gallery. It loads thumbnails,
+  updates the large image when clicked, and includes a button to darken or lighten the image.
+*/
+
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
-
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
