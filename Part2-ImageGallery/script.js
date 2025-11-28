3d8a1f09b3dba3d90505431b6f82ae10524c578b
rikumagar <84245137+rikumagar@users.noreply.github.com>
2025-11-27 23:06:53 -0500

    Second commit - This is the second commit with html header and comment

--- Part2-ImageGallery/script.js
+++ Part2-ImageGallery/script.js
@@ -1,3 +1,10 @@
+/* 
+Name: Hrigdev Thapa
+File: script.js
+Date: 27 November 2025
+Description: Starter JavaScript for the Image Gallery. This file will handle loading thumbnails, updating the main image, and toggling dark/light mode.
+*/
+
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
 
