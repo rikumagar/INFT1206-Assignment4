616ae585072cefc854ee766e397784659ee713ce
rikumagar <84245137+rikumagar@users.noreply.github.com>
2025-11-28 23:18:38 -0500

    Second commit - Added Header comments to the raw files

--- Part5-FormsChallenge/index.js
+++ Part5-FormsChallenge/index.js
@@ -1,1 +1,8 @@
+/*
+  Name: Hrigdev Thapa
+  Date:2025/11/28
+  Filename: index.js
+  Description: Empty JS file included because the starter template links to it.
+*/
+
 // intentionally left blank
